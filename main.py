207c7788474f25659745f6f5a1184c6c7e85cf67
ProjectsRO-hit <official.rohitsingh22@gmail.com>
2025-01-08 18:02:05 +0530

    updated

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 from turtle import Turtle, Screen
 import pandas as pd
+import time
 
-#setting screen
+#setting screen 1 Welcome!
 screen = Screen()
-screen.title("India States Game")
+screen.title("Welcome to Celibrity Quiz Game")  
+image = "welcome.gif"
+screen.addshape(image)
+turtle = Turtle()
+turtle.shape(image)
+time.sleep(5)
+
+screen = Screen()
+screen.title("Welcome to Celibrity Quiz Game")  
 image = "./pop_stars/1.gif"
 screen.addshape(image)
 
-#setting turtle
 turtle = Turtle()
 turtle.shape(image)
-#setting up pandas
+screen.textinput(title="Guess the Celebrity", prompt="Who are they?")
+
+
+
 
 
 screen.exitonclick()
