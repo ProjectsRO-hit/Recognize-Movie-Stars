b78f861001723a38a524fa2115d5cd4b9032aee5
ProjectsRO-hit <official.rohitsingh22@gmail.com>
2025-01-08 17:37:32 +0530

    changes

---
+++ main.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+import pandas as pd
+
+#setting screen
+screen = Screen()
+screen.title("India States Game")
+image = "./pop_stars/1.gif"
+screen.addshape(image)
+
+#setting turtle
+turtle = Turtle()
+turtle.shape(image)
+#setting up pandas
+
+
+screen.exitonclick()
